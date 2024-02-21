62457b8beb1134f85755440146f4bf53b7ae14ee
VMTask <congmingfan@outlook.com>
2024-02-21 14:24:05 +0800

    Update README,add config system

---
+++ interface/SettingsActivity.py
@@ -0,0 +1,213 @@
+# coding:utf-8
+from qfluentwidgets import (SettingCardGroup, SwitchSettingCard, FolderListSettingCard,
+                            OptionsSettingCard, RangeSettingCard, PushSettingCard,
+                            ColorSettingCard, HyperlinkCard, PrimaryPushSettingCard, ScrollArea,
+                            ComboBoxSettingCard, ExpandLayout, Theme, InfoBar, CustomColorSettingCard,
+                            setTheme, setThemeColor, isDarkTheme,QConfig)
+from qfluentwidgets import (qconfig, QConfig, ConfigItem, OptionsConfigItem, BoolValidator,
+                            ColorConfigItem, OptionsValidator, RangeConfigItem, RangeValidator,
+                            FolderListValidator, EnumSerializer, FolderValidator, ConfigSerializer, __version__)
+from qfluentwidgets import FluentIcon as FIF
+from PyQt5.QtCore import Qt, pyqtSignal, QUrl, QStandardPaths
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QWidget, QLabel, QFontDialog, QFileDialog, QFrame
+HELP_URL = ""
+YEAR = 114514
+AUTHOR = ""
+VERSION = "14"
+FEEDBACK_URL = ""
+
+class SettingInterface(ScrollArea):
+    """ Setting interface """
+
+    checkUpdateSig = pyqtSignal()
+    musicFoldersChanged = pyqtSignal(list)
+    acrylicEnableChanged = pyqtSignal(bool)
+    downloadFolderChanged = pyqtSignal(str)
+    minimizeToTrayChanged = pyqtSignal(bool)
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.scrollWidget = QWidget()
+        self.expandLayout = ExpandLayout(self.scrollWidget)
+        self.setObjectName("SettingInterface")
+        # setting label
+        self.settingLabel = QLabel(self.tr("设置"), self)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setStyleSheet("background: transparent;")
+        self.setFrameShape(QFrame.NoFrame)
+        self.settingLabel.setStyleSheet("font: 33px 'Microsoft YaHei Light';background-color: transparent;")
+
+
+        # personalization
+        self.personalGroup = SettingCardGroup(self.tr('Personalization'), self.scrollWidget)
+        """self.enableAcrylicCard = SwitchSettingCard(
+            FIF.TRANSPARENT,
+            self.tr("Use Acrylic effect"),
+            self.tr("Acrylic effect has better visual experience, but it may cause the window to become stuck"),
+            configItem=QConfig.enableAcrylicBackground,
+            parent=self.personalGroup
+        )"""
+        self.themeCard = OptionsSettingCard(
+            QConfig.themeMode,
+            FIF.BRUSH,
+            self.tr('Application theme'),
+            self.tr("Change the appearance of your application"),
+            texts=[
+                self.tr('Light'), self.tr('Dark'),
+                self.tr('Use system setting')
+            ],
+            parent=self.personalGroup
+        )
+        self.themeColorCard=CustomColorSettingCard(
+            QConfig.themeColor,
+            FIF.PALETTE,
+            self.tr('Theme color'),
+            self.tr('Change the theme color of you application'),
+            self.personalGroup
+        )
+
+
+        
+        # main panel
+        self.mainPanelGroup = SettingCardGroup(self.tr('Main Panel'), self.scrollWidget)
+        self.minimizeToTrayCard = SwitchSettingCard(
+            FIF.MINIMIZE,
+            self.tr('Minimize to tray after closing'),
+            self.tr('PyQt-Fluent-Widgets will continue to run in the background'),
+            configItem=ConfigItem(
+        "MainWindow", "MinimizeToTray", True, BoolValidator()),
+            parent=self.mainPanelGroup
+        )
+
+        # update software
+        self.updateSoftwareGroup = SettingCardGroup(self.tr("Software update"), self.scrollWidget)
+        self.updateOnStartUpCard = SwitchSettingCard(
+            FIF.UPDATE,
+            self.tr('Check for updates when the application starts'),
+            self.tr('The new version will be more stable and have more features'),
+            configItem=ConfigItem(
+        "Update", "CheckUpdateAtStartUp", True, BoolValidator()),
+            parent=self.updateSoftwareGroup
+        )
+
+        # application
+        self.aboutGroup = SettingCardGroup(self.tr('About'), self.scrollWidget)
+        self.helpCard = HyperlinkCard(
+            HELP_URL,
+            self.tr('Open help page'),
+            FIF.HELP,
+            self.tr('Help'),
+            self.tr('Discover new features and learn useful tips about PyQt-Fluent-Widgets'),
+            self.aboutGroup
+        )
+        self.feedbackCard = PrimaryPushSettingCard(
+            self.tr('Provide feedback'),
+            FIF.FEEDBACK,
+            self.tr('Provide feedback'),
+            self.tr('Help us improve PyQt-Fluent-Widgets by providing feedback'),
+            self.aboutGroup
+        )
+        self.aboutCard = PrimaryPushSettingCard(
+            self.tr('Check update'),
+            FIF.INFO,
+            self.tr('About'),
+            '© ' + self.tr('Copyright') + f" {YEAR}, {AUTHOR}. " +
+            self.tr('Version') + f" {VERSION}",
+            self.aboutGroup
+        )
+
+        self.__initWidget()
+
+    def __initWidget(self):
+        self.resize(1000, 800)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setViewportMargins(0, 120, 0, 20)
+        self.setWidget(self.scrollWidget)
+        self.setWidgetResizable(True)
+        # initialize style sheet
+        self.__setQss()
+
+        # initialize layout
+        self.__initLayout()
+        self.__connectSignalToSlot()
+
+    def __initLayout(self):
+        self.settingLabel.move(60, 63)
+
+        # add cards to group
+
+        self.personalGroup.addSettingCard(self.themeCard)
+        self.personalGroup.addSettingCard(self.themeColorCard)
+
+
+        self.updateSoftwareGroup.addSettingCard(self.updateOnStartUpCard)
+
+        self.mainPanelGroup.addSettingCard(self.minimizeToTrayCard)
+
+        self.aboutGroup.addSettingCard(self.helpCard)
+        self.aboutGroup.addSettingCard(self.feedbackCard)
+        self.aboutGroup.addSettingCard(self.aboutCard)
+
+        # add setting card group to layout
+        self.expandLayout.setSpacing(28)
+        self.expandLayout.setContentsMargins(60, 10, 60, 0)
+        self.expandLayout.addWidget(self.personalGroup)
+        self.expandLayout.addWidget(self.mainPanelGroup)
+        self.expandLayout.addWidget(self.updateSoftwareGroup)
+        self.expandLayout.addWidget(self.aboutGroup)
+
+    def __setQss(self):
+        """ set style sheet """
+        self.scrollWidget.setObjectName('scrollWidget')
+        self.settingLabel.setObjectName('settingLabel')
+
+
+    def __showRestartTooltip(self):
+        """ show restart tooltip """
+        InfoBar.warning(
+            '',
+            self.tr('Configuration takes effect after restart'),
+            parent=self.window()
+        )
+
+    def __onDeskLyricFontCardClicked(self):
+        """ desktop lyric font button clicked slot """
+        font, isOk = QFontDialog.getFont(
+            QConfig.desktopLyricFont, self.window(), self.tr("Choose font"))
+        if isOk:
+            QConfig.desktopLyricFont = font
+
+    def __onDownloadFolderCardClicked(self):
+        """ download folder card clicked slot """
+        folder = QFileDialog.getExistingDirectory(
+            self, self.tr("Choose folder"), "./")
+        if not folder or QConfig.get(QConfig.downloadFolder) == folder:
+            return
+
+        QConfig.set(QConfig.downloadFolder, folder)
+        self.downloadFolderCard.setContent(folder)
+
+    def __onThemeChanged(self, theme: Theme):
+        """ theme changed slot """
+        # change the theme of qfluentwidgets
+        setTheme(theme)
+
+
+    def __connectSignalToSlot(self):
+        """ connect signal to slot """
+        cfg = QConfig()
+        cfg.appRestartSig.connect(self.__showRestartTooltip)
+        cfg.themeChanged.connect(self.__onThemeChanged)
+
+
+        self.themeColorCard.colorChanged.connect(setThemeColor)
+
+        # main panel
+        self.minimizeToTrayCard.checkedChanged.connect(
+            self.minimizeToTrayChanged)
+
+        # about
+        self.aboutCard.clicked.connect(self.checkUpdateSig)
+        self.feedbackCard.clicked.connect(
+            lambda: QDesktopServices.openUrl(QUrl(FEEDBACK_URL)))
